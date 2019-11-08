894ad3e2e1299e9b95eca483f01faaf697f2a455
Joshua Miles <30753026+Joshua-Miles@users.noreply.github.com>
2019-11-08 11:24:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -36,18 +36,6 @@ setInterval(function(){
     if(direction == 'right'){
         character.style.left = `${left+speed}px`
     }
-    
-    if(direction == 'left'){
-        character.style.left = `${left-speed}px`
-    }
-    
-    if(direction == 'up'){
-        character.style.top = `${top-speed}px`
-    }
-    
-    if(direction == 'down'){
-        character.style.top = `${top+speed}px`
-    }
 
     // Account for other directions here:
 
@@ -60,24 +48,4 @@ setInterval(function(){
 function walkRight(){
     character.src = `${ASSET_ROOT}/walkright.gif`
     direction = 'right'
-}
-
-function walkLeft(){
-    character.src = `${ASSET_ROOT}/walkleft.gif`
-    direction = 'left'
-}
-
-function walkUp(){
-    character.src = `${ASSET_ROOT}/walkup.gif`
-    direction = 'up'
-}
-
-function walkDown(){
-    character.src = `${ASSET_ROOT}/walkdown.gif`
-    direction = 'down'
-}
-
-function stop(){
-    character.src = `${ASSET_ROOT}/static.gif`
-    direction = null
 }
